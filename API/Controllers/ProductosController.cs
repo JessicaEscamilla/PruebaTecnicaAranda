45b7d4ad37a619874ebc4cce0f87cfc5f7cacad8
JESSICA JINNETH ESCAMILLA CARRERO <jescamilla02@unisalle.edu.co>
2023-01-17 19:57:48 -0500

    Se elimino el controlador por defecto. Se creo el controlador productos

---
+++ API/Controllers/ProductosController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductosController : ControllerBase
+    {
+        [HttpGet(Name = "ObtenerProductos")]
+        public async Task<IActionResult> ObtenerProductos()
+        {
+            return Ok("Hola mundo");
+        }
+    }
+}
